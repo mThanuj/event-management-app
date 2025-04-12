da5070b38f7dfd57b2d9efdaebc5cfa700218b78
mthanuj <mullagurithanuj0@gmail.com>
2025-04-12 11:10:30 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const eventRoutes = require("./routes/events.route.js");
+require("dotenv").config();
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN,
   }),
 );
 app.use(express.json());
